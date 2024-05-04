47858e7f3c50caa1eb318fd653187d1e034ca535
aman-jain13 <ajain13032002@gmail.com>
2024-05-05 02:35:23 +0530

    adds infinite scrolling when page end reached

--- src/App.js
+++ src/App.js
@@ -1,37 +1,68 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Grid } from '@mui/material';
 import JobCard from './components/JobCard';
 import './App.css';
 
 const App = () => {
   const [jobData, setJobData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const page = useRef(1);
+  const loader = useRef(null);
+
+  const fetchJobs = async () => {
+    if (loading || !hasMore) return;
+
+    setLoading(true);
+
+    try {
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      const body = JSON.stringify({
+        "limit": 10,
+        "offset": (page.current - 1) * 10
+      });
+
+      const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body
+      };
+
+      const response = await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions);
+      const data = await response.json();
+
+      if (data.jdList.length === 0) {
+        setHasMore(false);
+      } else {
+        setJobData(prevJobs => [...prevJobs, ...data.jdList]);
+        page.current++;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
+    setLoading(false);
+  };
+
+  const handleScroll = () => {
+    if (loading || !hasMore) return;
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+  
+    if (scrollTop + clientHeight >= scrollHeight - 20) {
+      fetchJobs();
+    }
+  };
+  
 
   useEffect(() => {
-    const fetchJobs = async () => {
-      try {
-        const myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-
-        const body = JSON.stringify({
-          "limit": 10,
-          "offset": 0
-        });
-
-        const requestOptions = {
-          method: "POST",
-          headers: myHeaders,
-          body
-        };
-
-        const response = await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions);
-        const data = await response.json();
-        setJobData(data.jdList); // Update to match the property name in the JSON response
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
     fetchJobs();
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
   return (
@@ -41,6 +72,7 @@ const App = () => {
           <JobCard {...job} />
         </Grid>
       ))}
+      {loading && <div className="loading" ref={loader}>Loading...</div>}
     </Grid>
   );
 };
