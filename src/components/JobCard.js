bfedabe07ca9c7d8442a392969c2dd6848b12bc7
aman-jain13 <ajain13032002@gmail.com>
2024-05-05 00:23:02 +0530

    removes grid from JobCard and adds it to App

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,31 +1,31 @@
+// JobCard.js
+
 import React from 'react';
-import { Grid, Paper, CardContent, Typography, Box, Button } from '@mui/material';
+import { Paper, CardContent, Typography, Box, Button } from '@mui/material';
 
 const JobCard = ({ job }) => {
   return (
-    <Grid item>
-      <Paper>
-        <CardContent>
-          <Box>
-            <Typography>{job.company}</Typography>
-            <Typography>{job.title}</Typography>
-            <Typography>{job.location}</Typography>
-          </Box>
-          <Box>
-            <Typography>{job.description}</Typography>
-          </Box>
-          <Box>
-            <a href="#" target="_blank" rel="noopener noreferrer">View Job</a>
-          </Box>
-          <Box>
-            <Typography>Minimum Experience: {job.experience}</Typography>
-          </Box>
-        </CardContent>
-        <Box textAlign="center">
-          <Button variant="contained" color="primary">Apply</Button>
+    <Paper>
+      <CardContent>
+        <Box>
+          <Typography>{job.company}</Typography>
+          <Typography>{job.title}</Typography>
+          <Typography>{job.location}</Typography>
         </Box>
-      </Paper>
-    </Grid>
+        <Box>
+          <Typography>{job.description}</Typography>
+        </Box>
+        <Box>
+          <a href="#" target="_blank" rel="noopener noreferrer">View Job</a>
+        </Box>
+        <Box>
+          <Typography>Minimum Experience: {job.experience}</Typography>
+        </Box>
+      </CardContent>
+      <Box textAlign="center">
+        <Button variant="contained" color="primary">Apply</Button>
+      </Box>
+    </Paper>
   );
 };
 
